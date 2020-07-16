e7d97999d4ad4b774687e5646db1507d4719e55e
AI3 <mamesocute3@gmail.com>
2020-07-16 04:04:54 +0000

    Add index for email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200715035841) do
+ActiveRecord::Schema.define(version: 20200716035826) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20200715035841) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
