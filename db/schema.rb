06ed16adf7cd979bdfbf1b113ffcceca7d0ec2a6
AI3 <mamesocute3@gmail.com>
2020-08-02 15:26:44 +0000

    Finish working time display

--- db/schema.rb
+++ db/schema.rb
@@ -32,8 +32,8 @@ ActiveRecord::Schema.define(version: 20200728123833) do
     t.datetime "updated_at", null: false
     t.string "remember_digest"
     t.boolean "admin", default: false
-    t.datetime "basic_time", default: "2020-07-23 23:00:00"
-    t.datetime "work_time", default: "2020-07-23 22:30:00"
+    t.datetime "basic_time", default: "2020-08-01 22:30:00"
+    t.datetime "work_time", default: "2020-08-01 23:00:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
