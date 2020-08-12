ec527578cc3bf7136a5bf8b8665534f54d876914
AI3 <mamesocute3@gmail.com>
2020-08-12 15:48:52 +0000

    Finish search form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,7 @@ class UsersController < ApplicationController
   before_action :set_one_month, only: :show
   
   def index
-    @users = User.paginate(page: params[:page])
-  end
-  
-  def search
+    @users = User.paginate(page: params[:page]).search(params[:search])
   end
   
   def show
